683315e20a732444cdf6a83cc835a1cbb96bd5e8
Apoorv Taneja <purutaneja.com@gmail.com>
2022-06-26 20:09:59 +0530

    fixed styles import

--- pages/dashboard/view/[id].js
+++ pages/dashboard/view/[id].js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
 import axios from 'axios';
-import { ResponseContainer } from './styles';
+import { ResponseContainer } from '../../../styles/viewStyles';
 import Header from '../../../components/Navbar';
 import Table from '../../../components/Table';
 import ProtectedRoute from '../../../components/ProtectedRoute';

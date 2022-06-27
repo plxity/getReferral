b04606c37a227cf0dc14c6092bd9273329f9ad3e
Apoorv Taneja <purutaneja.com@gmail.com>
2022-06-28 02:25:36 +0530

    testin route

--- pages/api/test/index.js
+++ pages/api/test/index.js
@@ -6,7 +6,15 @@ import dbConnect from '../../../lib/connectMainDB';
 export default async function handler(req, res) {
   try {
     const PEERLIST_URL = process.env.PEERLIST_URL;
-    let peerlistDetails = await axios.get("https://rickandmortyapi.com/api/character/80");
+    let peerlistDetails = await axios.get(
+      `${PEERLIST_URL}username=plxity`,
+      {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
+      }
+    );
     return res.status(200).json({ status: peerlistDetails.data.data });
   } catch (err) {
     console.log(err);

de296c8b5818b056c4970e6c4245133cb33823ae
Apoorv Taneja <purutaneja.com@gmail.com>
2022-06-27 20:15:50 +0530

    attempt to make meta tags work

--- pages/meta.js
+++ pages/meta.js
@@ -4,24 +4,11 @@ export default function Meta() {
   return (
     <Head>
       <title>getReferral</title>
-      <meta name="title" content="c" />
       <meta
         name="description"
         content="Manage your referrals without any hassle and apply to job openings in one click powered by Peerlist"
       />
-      <meta itemprop="name" content="getReferral - referrals made easy" />
-      <meta
-        itemprop="description"
-        content="Manage your referrals without any hassle and apply to job openings in one click powered by Peerlist"
-      />
-      <meta
-        itemprop="image"
-        content="https://res.cloudinary.com/dhunsqg4j/image/upload/v1656337546/Group_102_wel9ok.png"
-      />
-
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://get-referral.vercel.app/" />
-      <meta property="og:title" content="getReferral - referrals made easy" />
+      <meta property="og:title" content="getReferral" />
       <meta
         property="og:description"
         content="Manage your referrals without any hassle and apply to job openings in one click powered by Peerlist"
@@ -30,18 +17,20 @@ export default function Meta() {
         property="og:image"
         content="https://res.cloudinary.com/dhunsqg4j/image/upload/v1656337546/Group_102_wel9ok.png"
       />
-
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://get-referral.vercel.app/" />
-      <meta property="twitter:title" content="getReferral - referrals made easy" />
       <meta
-        property="twitter:description"
+        property="og:url"
+        content="https://get-referral-git-add-meta-tags-plxity.vercel.app/"
+      />
+      <meta name="twitter:title" content="getReferral" />
+      <meta
+        name="twitter:description"
         content="Manage your referrals without any hassle and apply to job openings in one click powered by Peerlist"
       />
       <meta
-        property="twitter:image"
+        name="twitter:image"
         content="https://res.cloudinary.com/dhunsqg4j/image/upload/v1656337546/Group_102_wel9ok.png"
       />
+      <meta name="twitter:card" content="summary_large_image"></meta>
       <link rel="icon" href="/logo.svg" />
     </Head>
   );

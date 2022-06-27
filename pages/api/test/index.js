7527d8028e114e0a17bd28183791504c3f72aec0
Apoorv Taneja <purutaneja.com@gmail.com>
2022-06-28 01:36:45 +0530

    testing

--- pages/api/test/index.js
+++ pages/api/test/index.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 import dbConnect from '../../../lib/connectMainDB';
 
 
-dbConnect();
+// dbConnect();
 
 export default async function handler(req, res) {
   try {
+    const PEERLIST_URL = process.env.PEERLIST_URL;
     let peerlistDetails = await axios.get(`${PEERLIST_URL}username=plxity`);
-
-    return res.status(200).json({ status: peerlistDetails });
+    return res.status(200).json({ status: peerlistDetails.data.data });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: 'Something went wrong' });

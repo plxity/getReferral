9f38e4dd54e2fe41a16b118f6f0cda30ab7db300
Apoorv Taneja <purutaneja.com@gmail.com>
2022-06-28 01:42:07 +0530

    testing

--- pages/api/test/index.js
+++ pages/api/test/index.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 import dbConnect from '../../../lib/connectMainDB';
 
-
 // dbConnect();
 
 export default async function handler(req, res) {
   try {
     const PEERLIST_URL = process.env.PEERLIST_URL;
-    let peerlistDetails = await axios.get(`${PEERLIST_URL}username=plxity`);
+    let peerlistDetails = await axios.get(`${PEERLIST_URL}username=plxity`, {
+      headers: {
+        Accept: 'application/json',
+        'User-Agent': 'axios 0.21.1',
+      },
+    });
     return res.status(200).json({ status: peerlistDetails.data.data });
   } catch (err) {
     console.log(err);

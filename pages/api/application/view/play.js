564c6e915f0e441fb1e7b38b3ffcbea496a91f88
Apoorv Taneja <purutaneja.com@gmail.com>
2022-06-27 23:53:01 +0530

    remove unsed imports

--- pages/api/application/view/play.js
+++
@@ -1,9 +0,0 @@
-// }
-// console.log(fetchParticularApplication);
-// fetchParticularApplication = fetchParticularApplication[0];
-// const { applicationsCreated, userId } = fetchParticularApplication;
-// console.log(applicationsCreated, userId);
-// return res.status(200).json({
-//   application: applicationsCreated,
-//   userId: userId,
-// });

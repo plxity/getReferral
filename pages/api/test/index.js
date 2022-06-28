7f3c7be8439258497411f4def2749c9efb22eed0
Apoorv Taneja <purutaneja.com@gmail.com>
2022-06-28 17:01:15 +0530

    testing again

--- pages/api/test/index.js
+++ pages/api/test/index.js
@@ -2,13 +2,16 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   fetch('https://peerlist.io/api/v1/users/resume?username=plxity')
-    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
     .then((data) => {
       console.log(data);
-      return data;
+      return res.send(200);
     })
     .catch((err) => {
       console.log(err);
-      return err;
+      return res.send(400);
     });
 }

839bb127fd897d9eb1e8ec95f45568865fe919bf
Apoorv Taneja <purutaneja.com@gmail.com>
2022-06-28 15:32:50 +0530

    testing api again

--- pages/api/test/index.js
+++ pages/api/test/index.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-export default async function handler(req, res) {
-  try {
-    let peerlistDetails = await axios.get(
-      `https://peerlist.io/api/v1/users/resume?username=plxity`
-    );
-    return res.status(200).json({ status: peerlistDetails.data });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ error: err });
-  }
-}
+
+const handler = async (req, resp) => {
+	const response = await axios.get(
+		`https://peerlist.io/api/v1/users/resume?username=plxity`
+	);
+
+	resp.send({
+		success: true,
+		data: response.data,
+	});
+};
+
+export default handler;

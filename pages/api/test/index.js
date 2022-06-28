c47029255566426dc9f90d0bb3aad4b90cc4c014
Apoorv Taneja <purutaneja.com@gmail.com>
2022-06-28 15:24:09 +0530

    changed response

--- pages/api/test/index.js
+++ pages/api/test/index.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     let peerlistDetails = await axios.get(
       `https://peerlist.io/api/v1/users/resume?username=plxity`
     );
-    return res.status(200).json({ status: peerlistDetails.data.data });
+    return res.status(200).json({ status: peerlistDetails.data });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: err });

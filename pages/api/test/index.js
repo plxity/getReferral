a3cc7e29036ec3f712c7367677c3d2d3afdc3a3f
Apoorv Taneja <purutaneja.com@gmail.com>
2022-06-28 17:15:32 +0530

    testing again

--- pages/api/test/index.js
+++ pages/api/test/index.js
@@ -6,6 +6,10 @@ export default async function handler(req, res) {
       `https://peerlist.io/api/v1/users/resume?username=plxity`,
       {
         crossDomain: true,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
       }
     );
     return res.status(200).json({ status: peerlistDetails.data });
